0418a44c785cabf04b68fe4a9dd151b3d7868f1d
Sheelansu <sheelansu.ujjain@hotmail.com>
2026-06-17 22:51:53 +0530

    prisma setup and some logic in signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,26 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', async (c) => {
+  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c)
+
+  const prisma = new PrismaClient({
+      datasourceUrl: DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+  await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password
+    }
+  })
 })
+
 
 app.post('/api/v1/signin', (c) => {
   return c.text('Hello Hono!')
@@ -18,7 +34,7 @@ app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
 
-app.get('/api/v1/blog', (c) => {
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
