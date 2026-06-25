bfbb93030008b42c48612e304d96bab666382574
Sheelansu <sheelansu.ujjain@hotmail.com>
2026-06-26 01:53:01 +0530

    pages added & cors fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
-import { authMiddleware } from "./middleware/authMiddleware";
 import { authAcountRouter } from "./routes/auth.account";
 import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 
 
 const app = new Hono();
+
+app.use('/api/*', cors())
 
 app.route("/api/v1/user", authAcountRouter)
 app.route("/api/v1/blog", blogRouter)
